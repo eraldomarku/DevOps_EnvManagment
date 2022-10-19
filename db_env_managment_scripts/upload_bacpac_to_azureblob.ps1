d66694c41211e8837abc6691ca434c9bc4b03a8f
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-19 12:56:33 +0200

    Create upload_bacpac_to_azureblob.ps1

---
+++ db_env_managment_scripts/upload_bacpac_to_azureblob.ps1
@@ -0,0 +1,1 @@
+-SourceUri "C:\Bacpacs\db_dev_1.bacpac" -DestinationUri "https://apsiaem01ada09a20ec1a713.blob.core.windows.net/retail-cdx-down-68719476736?sp=racwdli&st=2022-10-19T10:51:08Z&se=2023-02-02T19:51:08Z&spr=https&sv=2021-06-08&sr=c&sig=kiGgyozma%2FZWom%2BtWz%2F7ynf8rEoXCn3XP5DjigiS65Q%3D" -ShowOriginalProgress
