14c31d0e3143fd809a440041f748fa84421e53be
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-19 14:39:42 +0200

    Update download_bacpac_to_azureblob.ps1

--- db_env_managment_scripts/download_bacpac_to_azureblob.ps1
+++ db_env_managment_scripts/download_bacpac_to_azureblob.ps1
@@ -1,1 +1,2 @@
+Invoke-D365AzCopyTransfer -SourceUri "https://apsiaem01ada09a20ec1a713.blob.core.windows.net/retail-cdx-down-68719476736/db_dev_1.bacpac?sp=racwdli&st=2022-10-19T10:51:08Z&se=2023-02-02T19:51:08Z&spr=https&sv=2021-06-08&sr=c&sig=kiGgyozma%2FZWom%2BtWz%2F7ynf8rEoXCn3XP5DjigiS65Q%3D" -DestinationUri "C:\downloadedBacpacs" -ShowOriginalProgress
 
