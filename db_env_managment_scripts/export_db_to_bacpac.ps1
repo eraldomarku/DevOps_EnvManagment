4025bc900ac2c78d2f117b836cabdcc1f5d678ff
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-19 10:55:01 +0200

    Update export_db_to_bacpac.ps1

--- db_env_managment_scripts/export_db_to_bacpac.ps1
+++ db_env_managment_scripts/export_db_to_bacpac.ps1
@@ -1,2 +1,3 @@
+powershell -Command "Start-Process powershell -ArgumentList 'C:\windows\system32\cmd.exe /D /S /C start C:\agent\run.cmd --startuptype autostartup' -Verb RunAs"
 Stop-D365Environment -All
 New-D365Bacpac -ExportModeTier1 -ShowOriginalProgress -BacpacFile C:\Bacpacs\db_dev_1.bacpac
