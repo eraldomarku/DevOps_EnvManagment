28e7a817bae90c8adf2d4ebf8d439c025af46158
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-19 16:09:48 +0200

    Update install_env_dependencies.ps1

--- db_env_managment_scripts/install_env_dependencies.ps1
+++ db_env_managment_scripts/install_env_dependencies.ps1
@@ -1,4 +1,9 @@
--Install-Module -Name d365fo.tools
--Invoke-D365InstallSqlPackage
-Set-ExecutionPolicy Unrestricted
-Invoke-D365InstallAzCopy
+if (Get-Module -ListAvailable -Name d365fo.tools) {
+    Set-ExecutionPolicy Unrestricted
+} 
+else {
+    Install-Module -Name d365fo.tools
+    Invoke-D365InstallSqlPackage
+    Set-ExecutionPolicy Unrestricted
+    Invoke-D365InstallAzCopy
+}
