15de4ddce4a5ccadcb259f0d15d07b30cb470779
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-20 10:24:51 +0200

    Update install_env_dependencies.ps1

--- db_env_managment_scripts/install_env_dependencies.ps1
+++ db_env_managment_scripts/install_env_dependencies.ps1
@@ -2,6 +2,8 @@ if (Get-Module -ListAvailable -Name d365fo.tools) {
     Set-ExecutionPolicy Unrestricted
 } 
 else {
+    Import-Module -Name PowerShellGet -Force
+    Import-Module -Name PackageManagement -Force
     Install-Module -Name d365fo.tools
     Invoke-D365InstallSqlPackage
     Set-ExecutionPolicy Unrestricted
