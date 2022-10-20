43c22af36aa254688b2476cccd9802f2df1440bf
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-20 15:04:29 +0200

    Update import_db_from_bacpac.ps1

--- db_env_managment_scripts/import_db_from_bacpac.ps1
+++ db_env_managment_scripts/import_db_from_bacpac.ps1
@@ -1,4 +1,6 @@
-Import-D365Bacpac -ImportModeTier1 -BacpacFile "C:\downloadedBacpacs\db_dev_1.bacpac" -NewDatabaseName AxDB_updated
+$bacpac_file_path = "C:\downloadedBacpacs\db_dev_1.bacpac"
+
+Import-D365Bacpac -ImportModeTier1 -BacpacFile $bacpac_file_path -NewDatabaseName AxDB_updated
 Stop-D365Environment -All
 Switch-D365ActiveDatabase -SourceDatabaseName AxDB_updated
 Invoke-D365DBSync -ShowOriginalProgress
