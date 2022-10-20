8361270582d0adaf67d613a4a7ef63bdac6d9fcc
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-20 14:49:50 +0200

    Update export_db_to_bacpac.ps1

--- db_env_managment_scripts/export_db_to_bacpac.ps1
+++ db_env_managment_scripts/export_db_to_bacpac.ps1
@@ -1,3 +1,14 @@
-Stop-D365Environment -All
-New-D365Bacpac -ExportModeTier1 -ShowOriginalProgress -BacpacFile C:\Bacpacs\db_dev_1.bacpac
-Start-D365Environment -All
+$bacpacs_path = "C:\Bacpacs"
+$bacpac_file_path = "C:\Bacpacs\db_dev_1.bacpac"
+if (Test-Path $bacpacs_path) {
+    if (Test-Path $bacpac_file_path) {
+        Write-Host "Removing File That Also Exists"
+        Remove-Item $bacpac_file_path -Force
+        New-D365Bacpac -ExportModeTier1 -ShowOriginalProgress -BacpacFile $bacpac_file_path
+    }
+}
+else
+{
+    New-Item $bacpacs_path -itemType Directory
+    New-D365Bacpac -ExportModeTier1 -ShowOriginalProgress -BacpacFile $bacpac_file_path
+}
