f8f5fa2ae3c2b0bd4fdb0cefa953b6423c966c5c
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-24 16:31:38 +0200

    update

--- db_env_managment_scripts/import_db.ps1
+++ db_env_managment_scripts/import_db.ps1
@@ -2,11 +2,21 @@ $db_path=$args[0]
 $db_name=$args[1]
 $sql_db_name=$args[2]
 
-# Removes db from sql server if it actually exists
-Remove-D365Database -DatabaseName "${sql_db_name}"
-# Import db bacpac into new database and then synchronize the imported db
+# Check if sql db name also exists and removes it if is verificated
+if(Get-D365Database $sql_db_name){
+    Remove-D365Database -DatabaseName $sql_db_name    
+}
+# The switch command will copy the original AxDB as AxDB_original and if it exist we will have an error so we must remove it
+if(Get-D365Database AxDB_original){
+    Remove-D365Database -DatabaseName AxDB_original
+}
+# Import db bacpac into "sql_db_name" database created
 Import-D365Bacpac -ImportModeTier1 -BacpacFile "${db_path}${db_name}" -NewDatabaseName ${sql_db_name}
-#Stop-D365Environment -All
-#Switch-D365ActiveDatabase -SourceDatabaseName $sql_db_name
-#Invoke-D365DBSync -ShowOriginalProgress
-#Start-D365Environment -All
+# We need to stop all D365FO related services, to ensure that our D365FO database isn't being lock when we are going to update it.
+Stop-D365Environment -All
+# With the newly created "sql_db_name" database, we will be switching it in as the D365FO database
+Switch-D365ActiveDatabase -SourceDatabaseName "$sql_db_name"
+# We need to ensure that the codebase and the database is synchronized correctly and fully working
+Invoke-D365DBSync -ShowOriginalProgress
+# With the synchronization of the database completed, we need to start all D365FO related services again, to make the D365FO environment available again
+Start-D365Environment -All
