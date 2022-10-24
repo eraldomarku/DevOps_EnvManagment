aa373297f9f9caf4ef11888c1aa873f772e070ca
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-24 09:10:35 +0200

    upload fixed

--- db_env_managment_scripts/upload_db.ps1
+++ db_env_managment_scripts/upload_db.ps1
@@ -5,5 +5,5 @@ $db_path=$args[0]
 $db_name=$args[1]
 $blob_url=$args[2]
 
-# If the file exists it will replace it
-Invoke-D365AzCopyTransfer -SourceUri "${db_path}${db_name}" -DestinationUri $blob_url -ShowOriginalProgress -Force
+# If the file exists it will be replaced
+Invoke-D365AzCopyTransfer -SourceUri "${db_path}${db_name}" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
