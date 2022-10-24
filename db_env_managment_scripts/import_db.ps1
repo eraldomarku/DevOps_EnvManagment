2764c3a48680431ea438c7f4f5901051b6db5b54
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-24 14:38:43 +0200

    update

--- db_env_managment_scripts/import_db.ps1
+++ db_env_managment_scripts/import_db.ps1
@@ -2,8 +2,10 @@ $db_path=$args[0]
 $db_name=$args[1]
 $sql_db_name=$args[2]
 
-#Import db bacpac into new database and then synchronize the imported db
-Import-D365Bacpac -ImportModeTier1 -BacpacFile "${db_path}${db_name}" -NewDatabaseName $sql_db_name
+# Removes db from sql server if it actually exists
+Remove-D365Database -DatabaseName "${sql_db_name}"
+# Import db bacpac into new database and then synchronize the imported db
+Import-D365Bacpac -ImportModeTier1 -BacpacFile "${db_path}${db_name}" -NewDatabaseName $sql_db_name -Force
 Stop-D365Environment -All
 Switch-D365ActiveDatabase -SourceDatabaseName $sql_db_name
 Invoke-D365DBSync -ShowOriginalProgress
