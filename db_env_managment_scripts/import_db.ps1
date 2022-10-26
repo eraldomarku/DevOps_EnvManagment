d360edabc8129aeb4fdac50047b8e80671cb9011
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-26 10:46:28 +0200

    import test

--- db_env_managment_scripts/import_db.ps1
+++ db_env_managment_scripts/import_db.ps1
@@ -1,33 +1,30 @@
 $db_path=$args[0] 
 $db_name=$args[1]
 $sql_db_name=$args[2]
-$MSSQL_DATA_path=$args[3]
+#$MSSQL_DATA_path=$args[3]
 
-# Check if sql db name also exists and removes it if is verificated
-if(Get-D365Database $sql_db_name){
-    Remove-D365Database -DatabaseName $sql_db_name    
-}
-# The switch command will copy the original AxDB as AxDB_original and if it exist we will have an error so we should remove it
-if(Get-D365Database AxDB_original){
-    Remove-D365Database -DatabaseName AxDB_original
-}
 
-# When creating the new sql db where we will import new db the (sql_db_name)_Primary.mdf should not exist so we check and eventually remove it
-# It could be generated in case of interrupted import 
-#if(Test-Path "${MSSQL_DATA_path}${sql_db_name}_Primary.mdf"){
-#    # To remove this file is needed to stop sqlserver.exe service
-#    stop-service -name MSSQLSERVER -PassThru
-#    Remove-Item "${MSSQL_DATA_path}${sql_db_name}_Primary.mdf" -Force
-#    start-service -name MSSQLSERVER -PassThru
-#}
-
-# Import db bacpac into "sql_db_name" database created
-Import-D365Bacpac -ImportModeTier1 -BacpacFile "${db_path}${db_name}" -NewDatabaseName "${sql_db_name}"
 # We need to stop all D365FO related services, to ensure that our D365FO database isn't being lock when we are going to update it.
 Stop-D365Environment -All
-# With the newly created "sql_db_name" database, we will be switching it in as the D365FO database
-Switch-D365ActiveDatabase -SourceDatabaseName "$sql_db_name"
+
+# Remove actual AxDB database
+#if(Get-D365Database AxDB){
+#    Remove-D365Database -DatabaseName AxDB_updated    
+#}
+
+# Remove if there is a copy of db made by switch command
+#if(Get-D365Database AxDB_original){
+#    Remove-D365Database -DatabaseName AxDB_original    
+#}
+
+# Import bacpac db to new db
+Import-D365Bacpac -ImportModeTier1 -BacpacFile "${db_path}${db_name}" -NewDatabaseName $sql_db_name
+
+# With the newly created "AxDB" database, we will be switching it in as the D365FO database
+#Switch-D365ActiveDatabase -SourceDatabaseName "$sql_db_name"
+
 # We need to ensure that the codebase and the database is synchronized correctly and fully working
-Invoke-D365DBSync -ShowOriginalProgress
+#Invoke-D365DBSync -ShowOriginalProgress
+
 # With the synchronization of the database completed, we need to start all D365FO related services again, to make the D365FO environment available again
-Start-D365Environment -All
+#Start-D365Environment -All
