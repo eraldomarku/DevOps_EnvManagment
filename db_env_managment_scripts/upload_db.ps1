30f28a8f18cc60c3020368bd5096bd298505e883
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-10-26 21:03:33 +0200

    Delete db_env_managment_scripts directory

--- db_env_managment_scripts/upload_db.ps1
+++
@@ -1,9 +0,0 @@
-# First argument is the bacpacs path
-# Second argument is name of the db file
-# Third argument is the url of the blob to store the db
-$db_path=$args[0] 
-$db_name=$args[1]
-$blob_url=$args[2]
-
-# If the file exists it will be replaced
-Invoke-D365AzCopyTransfer -SourceUri "${db_path}${db_name}" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
