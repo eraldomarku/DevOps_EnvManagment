f13b559949330c5f973a78585592ee2ca1029e31
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-11-23 12:00:41 +0100

    update prova

--- db_env_managment_scripts/prova.ps1
+++ db_env_managment_scripts/prova.ps1
@@ -1,8 +1,8 @@
-$video_or_screenshot = args[0]
-$blob_url = $args[1]
-$ticked_id = $args[2]
-$url = $args[3]
+# Replace & with %26
 
+#$url = "https://devtestgiliebe02a3ff67b3b5edevaos.axcloud.dynamics.com/?cmp=ITCO&mi=DefaultDashboard"
+$url = "https:www.google.com"
+$url = $url.replace("&","%26")
 
 $token_set = @{
      L = [Char[]]'abcdefghijklmnopqrstuvwxyz'
@@ -14,7 +14,7 @@ $Number = Get-Random -Count 10 -InputObject $token_set.N
 
 $StringSet = $Lower + $Number 
 
-$video_input = $ticked_id
+$video_input = (Get-Random -Count 20 -InputObject $StringSet) -join ''
 $output_type = ".avi"
 
 $ffmpeg_output = $video_input + $output_type
@@ -52,4 +52,4 @@ $video_recording_path = $compressed_video
 
 #$blob_url = "https://apsialn010318c6c1d521243.blob.core.windows.net/source?sp=rw&st=2022-11-17T09:52:17Z&se=2022-11-17T17:52:17Z&spr=https&sv=2021-06-08&sr=c&sig=qlg6WXKXfkHvs4sl3FU3P%2FDkghju2GXYTVAyO6Dqp04%3D"
 #Invoke-D365AzCopyTransfer -SourceUri "$reprosteps_path" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
-Invoke-D365AzCopyTransfer -SourceUri "$video_recording_path" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
+#Invoke-D365AzCopyTransfer -SourceUri "$video_recording_path" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
