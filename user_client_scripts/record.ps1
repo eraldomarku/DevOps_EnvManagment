5f5a77cf7f467aa187a6b18f8030a254d9d34eaf
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-11-24 16:17:03 +0100

    v

--- user_client_scripts/record.ps1
+++ user_client_scripts/record.ps1
@@ -64,7 +64,7 @@ if("Video" -eq $video_or_screenshot){
 
 }
 
-if ("Screenshot" -eq $video_or_screenshot) {
+if ("Video" -eq $video_or_screenshot) {
     do{ Write-Output "When your page is ready, press F2 to take a screenshot.";
     $x = [System.Console]::ReadKey() 
     } while( $x.Key -ne "f2" )
