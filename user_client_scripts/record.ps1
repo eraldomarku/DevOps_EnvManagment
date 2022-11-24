cad1e7ccfe17f8299b91333e702d6690a95013e0
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-11-24 16:53:16 +0100

    v

--- user_client_scripts/record.ps1
+++ user_client_scripts/record.ps1
@@ -64,7 +64,7 @@ if("Video" -eq $video_or_screenshot){
 
 }
 
-if ("Video" -eq $video_or_screenshot) {
+if ("Screenshot" -eq $video_or_screenshot) {
     
     $screenshot_input = $ticked_id
     $output_type = ".jpeg"
