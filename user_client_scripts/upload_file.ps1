29c8257d5ea4e3da3e637551706032f6b8fe248b
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-11-28 17:44:21 +0100

    v

--- user_client_scripts/upload_file.ps1
+++ user_client_scripts/upload_file.ps1
@@ -1,7 +1,7 @@
 $ticked_id = $args[0]
 $blob_url = $args[1]
 
-$current_path= [System.Environment]::CurrentDirectory
+$current_path= Get-Location
 azcopy copy "${current_path}\${ticked_id}.zip" "$blob_url" --recursive=true
 
 #Invoke-D365AzCopyTransfer -SourceUri "${current_path}/${ticked_id}.zip" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
