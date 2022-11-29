beac7d443adefb68737bb9c9091862183655a677
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2022-11-29 10:36:55 +0100

    v

--- user_client_scripts/upload_file.ps1
+++ user_client_scripts/upload_file.ps1
@@ -7,7 +7,7 @@ $current_path= Get-Location
 azcopy copy "${current_path}\${ticket_id}.zip" "$blob_url" --recursive=true
 
 #Remove-Item  "$current_path\user_client_scripts\reprosteps.test.js"
-Remove-Item "$ticked_id.zip"
+#Remove-Item "$ticked_id.zip"
 
 
 #Invoke-D365AzCopyTransfer -SourceUri "${current_path}/${ticked_id}.zip" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
