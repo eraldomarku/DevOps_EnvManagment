1fcc295f1c60a26fd60e978779b93f622dde59d8
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2023-01-19 00:09:55 +0100

    added ansible

--- ansible_db_env_management_scripts/set_environments.ps1
+++ ansible_db_env_management_scripts/set_environments.ps1
@@ -1,8 +1,8 @@
-//Questo attiverà il servizio WinRM e imposterà alcune configurazioni di base.
+#Questo attiverà il servizio WinRM e imposterà alcune configurazioni di base.
 winrm quickconfig
-//per abilitare l'autenticazione di base.
+#per abilitare l'autenticazione di base
 winrm set winrm/config/service/auth @{Basic="true"}
-//per consentire la trasmissione di dati non crittografati.
+#per consentire la trasmissione di dati non crittografati.
 winrm set winrm/config/service @{AllowUnencrypted="true"}
-//per impostare la quantità massima di memoria per shell.
+#per impostare la quantità massima di memoria per shell.
 winrm set winrm/config/winrs @{MaxMemoryPerShellMB="512"}
