5d26fdf5c061f84d9faa8e4c98e5af505398a6b3
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2023-01-24 00:15:36 +0100

    m

--- ansible_db_env_management_scripts/set_azure_In_nsg_rule.ps1
+++ ansible_db_env_management_scripts/set_azure_In_nsg_rule.ps1
@@ -1,13 +1,12 @@
 param([string]$resourceGroupName,[string]$networkSecurityGroupName,[string]$ruleName,[string]$destinationPort)
-Write-Host "PASSO 1"
+
 $nsg_obj = Get-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName
-Write-Host "PASSO 2"
-$nsg_rule_obj = Get-AzNetworkSecurityRuleConfig -Name $ruleName -NetworkSecurityGroup $nsg_obj
-Write-Host "PASSO 3"
-if($nsg_rule_obj){
-    Write-Host "Rule "$ruleName" already exists" 
-}
-else{
+
+try{
+    $nsg_rule_obj = Get-AzNetworkSecurityRuleConfig -Name $ruleName -NetworkSecurityGroup $nsg_obj    
     Add-AzNetworkSecurityRuleConfig -Name $ruleName -NetworkSecurityGroup $nsg_obj -Protocol $protocol -SourcePortRange "*" -DestinationPortRange $destinationPort -SourceAddressPrefix "*" -DestinationAddressPrefix "*" -Direction "Inbound" -Access "Allow" -Priority 100
     Set-AzNetworkSecurityGroup -NetworkSecurityGroup $nsg_obj
+}
+catch{
+    Write-Host "Rule "$ruleName" already exists"
 }
