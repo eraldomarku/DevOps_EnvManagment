7039567b77ff91e450792cdee174f0db277579b6
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2023-01-26 18:25:55 +0100

    m

--- db_env_managment_scripts/upload_db.ps1
+++ db_env_managment_scripts/upload_db.ps1
@@ -5,5 +5,7 @@ $db_path=$args[0]
 $db_name=$args[1]
 $blob_url=$args[2]
 
+Write-Host "SCRIPTTTTT MODIFICATOOOOOOOO"
+
 # If the file exists it will be replaced
 Invoke-D365AzCopyTransfer -SourceUri "${db_path}${db_name}" -DestinationUri "$blob_url" -ShowOriginalProgress -Force
