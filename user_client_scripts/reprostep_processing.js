4086dd7f6c01db8c34ceff8f72aa5341c5072494
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2023-01-30 15:45:02 +0100

    m

--- user_client_scripts/reprostep_processing.js
+++ user_client_scripts/reprostep_processing.js
@@ -3,73 +3,75 @@ const fs = require('fs');
 const fileName = "reprosteps.test.js";
 const fileData = fs.readFileSync(fileName, "utf-8");
 
+// Split in lines
+
 var lines = fileData.split("\n");
 var urls = [];
-var timestamp;
 
 lines.forEach(function(line) {
-if (line.includes("await")) {
-if (line.includes("https://")){
-urls.push(line);
-}
-}
-if (line.includes("console.log")) {
-if (line.includes("Timestamp")) {
-timestamp = line.split("Timestamp: ")[1].replace("\r", "");
-}
-}
+    if (line.includes("await")) {
+        if (line.includes("https://")){
+            urls.push(line);
+        }
+    }
 })
 
 var fields = [];
 
+// Retrieve all query strings (after the question mark)
+
 urls.forEach(function(url) {
-if(url.includes("/?")){
-var parameters = url.split("/?");
-var body = parameters[1];
-if(body.includes("%26")){
-var str = body.split("%26");
-}
-else {
-var str = body.split("&");
-}
-str.forEach(function(s){
-var value = s.split("=")[1];
-fields.push(value.replace(/\W/g, ''));
-})
-}
+    if(url.includes("/?")){
+        var parameters = url.split("/?");
+        var body = parameters[1];
+        if(body.includes("%26")){
+            var str = body.split("%26");
+        }
+        else {
+            var str = body.split("&");
+        }
+        str.forEach(function(s){
+            var value = s.split("=")[1];
+            fields.push(value.replace(/\W/g, ''));
+        })
+    }
 })
 
+// This structure will host table names that can be retrieven from the urls.
+
 let uniqueFields = [...new Set(fields)];
+
+// Each type of accessed element: let's start from the buttons.
 
 var treeItems = [];
 
 lines.forEach(function(line) {
-if (line.includes("await")) {
-if (line.includes("getByRole")){
-if(line.includes('treeitem')){
-treeItems.push(line);
-}
-}
-}
+    if (line.includes("await")) {
+        if (line.includes("getByRole")){
+            if(line.includes('treeitem')){ // Treeitems
+                treeItems.push(line);
+            }
+        }
+    }
 })
 
-var regExp = /{([^)]+)}/;
+var regExp = /\{([^)]+)\}/;
 var accessed_treeItemElements = []
 
 treeItems.forEach(function(treeItem) {
-var info_item = regExp.exec(treeItem);
-var info_item_name = info_item[1].split(":")[1];
-var raw_info_item_name = info_item_name.replace(/["'"]/g, "").trim().replace(/[^\p{L}][ ]/gu, '').toLowerCase().replaceAll(' ', "_");
-if(raw_info_item_name != "modules"){
-accessed_treeItemElements.push(raw_info_item_name);
-}
+    var info_item = regExp.exec(treeItem);
+    var info_item_name = info_item[1].split(":")[1];
+    var raw_info_item_name = info_item_name.replace(/["'"]/g, "").trim().replace(/[^\p{L}][ ]/gu, '').toLowerCase().replaceAll(' ', "_");
+    if(raw_info_item_name != "modules"){
+        accessed_treeItemElements.push(raw_info_item_name);
+    }
 })
 
 let unique_treeItemElements = [...new Set(accessed_treeItemElements)];
 let features = uniqueFields.concat(unique_treeItemElements);
 
 features.forEach(function(feature) {
-fs.appendFile('reprosteps.txt', feature + "," + timestamp + "\n", (err) => {
-if (err) throw err;
-})
+    fs.appendFile('reprosteps.txt', feature + ",", (err) => {
+        if (err) throw err;
+    })
 })
