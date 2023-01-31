b69d9057eb4880d75c36b198c64f15cadfdd555e
eraldomarku <33416336+eraldomarku@users.noreply.github.com>
2023-02-01 00:50:26 +0100

    m

--- user_client_scripts/record.ps1
+++ user_client_scripts/record.ps1
@@ -132,16 +132,17 @@ if("Video" -eq $video_or_screenshot){
                 ) 
                 & ([scriptblock]::Create($ScriptBlock))} -ArgumentList $ffmpeg_command
 
+        #JOB EXECUTING LISTENER THAT SAVES TIME
         $current_path= Get-Location
-        $listener_command = "& '${current_path}\user_client_scripts\reprostep_file_listener.ps1' -reprostep_file_js ${current_path}\reprosteps.test.js"
-        $time_listener_reprostep = Start-ThreadJob -Name "time_listener_reprostep" -ScriptBlock { param (
-                [parameter(Mandatory=$true)][string]$ScriptBlock
-                ) 
-                & ([scriptblock]::Create($ScriptBlock))} -ArgumentList $listener_command
+        $scriptPath = "${current_path}\user_client_scripts\reprostep_file_listener.ps1"
+        $argPath = "${current_path}\reprosteps.test.js"
+        $listener_job = Start-Job -ScriptBlock {
+            & $using:scriptPath -reprostep_file_js $using:argPath
+        }
     }
 
     Wait-Job "play"
-    Stop-Job "time_listener_reprostep"
+    Stop-Job -Job $listener_job
     Stop-Job "video"
     & "${current_path}\user_client_scripts\reprostep_json_create.ps1" -reprostep_file_js "${current_path}\reprosteps.test.js" -ticket_id "$ticket_id"
     
